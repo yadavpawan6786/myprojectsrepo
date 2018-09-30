cf102eca6dc81d1622a74885c4e33703e65f6790
pawan <pawan@DESKTOP-OPKCHAE>
2018-09-30 16:47:43 +0530

    First Commit

---
+++ src/main/java/com/springsecurityoauthexample/SpringSecurityOauthExampleApplication.java
@@ -0,0 +1,12 @@
+package com.springsecurityoauthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauthExampleApplication.class, args);
+	}
+}
